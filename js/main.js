f37b09f9f768a73c8326ab46ac6a093c9b4ec3f3
razor262 <24.work@bk.ru>
2021-12-06 14:29:49 +0300

    Шапка сайта

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function(){
 
+    $('.header-slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
+        vertical: true
+    });
+
 });
